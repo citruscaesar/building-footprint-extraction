d49d376d7047e3e7ffa5ee378b7470d2e29d2392
Sambhav Chandra <vader.sam10@gmail.com>
2023-04-16 04:01:46 +0530

    modified for eval testing

---
+++ src/lightningmodule.py
@@ -0,0 +1,70 @@
+import torch
+import pytorch_lightning as pl
+
+import segmentation_models_pytorch as smp
+
+import torchmetrics 
+from torchmetrics import MetricCollection
+from torchmetrics.classification import BinaryJaccardIndex, BinaryF1Score, BinaryAccuracy
+
+class SegmentationModule(pl.LightningModule):
+    def __init__(self, model, preprocess, loss, learning_rate = 3e-4, batch_size = 32):
+        super().__init__()
+        self.model = model
+        self.preprocess_fn = preprocess
+
+        assert loss in ["dice", "iou", "bce"]
+        if loss == "dice":
+            self.loss = smp.losses.DiceLoss(mode = "binary")
+        elif loss == "iou":
+            self.loss = smp.losses.JaccardLoss(mode = "binary")
+        elif loss == "bce":
+            self.loss = smp.losses.SoftBCEWithLogitsLoss()
+
+        self.save_hyperparameters(ignore = ["model", "preprocess"])
+
+        self.val_metrics = MetricCollection([
+            BinaryF1Score(),
+            BinaryJaccardIndex(),
+            BinaryAccuracy()
+        ], prefix = "Validation ")
+
+        self.test_metrics = MetricCollection([
+            BinaryF1Score(),
+            BinaryJaccardIndex(),
+            BinaryAccuracy()
+        ], prefix = "Test ")
+
+
+    def forward(self, batch):
+        mask = self.model(batch) 
+        return mask
+        
+    def training_step(self, batch, batch_idx):
+        scene, mask = batch
+        pred_mask = self.model(scene)
+        loss = self.loss(pred_mask, mask) 
+        self.log("train/loss", loss.item(), on_epoch = True, on_step=True, prog_bar=True)
+        return loss
+    
+    def validation_step(self, batch, batch_idx):
+        scene, mask = batch
+        pred_mask = self.model(scene)
+        loss = self.loss(pred_mask, mask)
+        self.log("val/loss", loss.item(), on_epoch=True)
+
+        self.val_metrics.update(pred_mask, mask)
+        self.log_dict(self.val_metrics, on_epoch=True)
+
+    def test_step(self, batch, batch_idx):
+        scene, mask = batch
+        pred_mask = self.model(scene)
+        loss = self.loss(pred_mask, mask)
+        self.log("test/loss", loss.item(), on_epoch=True)
+
+        self.test_metrics.update(pred_mask, mask)
+        self.log_dict(self.test_metrics, on_epoch=True)
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.Adam(self.model.parameters(), lr = self.hparams.learning_rate)
+        return optimizer
